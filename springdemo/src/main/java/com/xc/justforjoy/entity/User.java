40424375190f681ac747ec7b1c8e7b047f2afc11
lxcecho <909231497@qq.com>
2021-01-08 16:59:35 +0800

    【add】spring 概念详解测试代码

--- springdemo/src/main/java/com/xc/justforjoy/entity/User.java
+++ springdemo/src/main/java/com/xc/justforjoy/entity/User.java
@@ -5,4 +5,28 @@ package com.xc.justforjoy.entity;
  * @since 2021/1/7
  */
 public class User {
+
+	private String name;
+
+	private String value;
+
+	public void init() {
+		System.out.println("init...");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
 }
