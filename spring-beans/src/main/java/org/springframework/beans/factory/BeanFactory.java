c0bf9d6a7263f40b64b065cf69406b35fac5d30a
lxcecho <909231497@qq.com>
2021-01-14 11:45:13 +0800

    upd：修改错别字

--- spring-beans/src/main/java/org/springframework/beans/factory/BeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -124,7 +124,7 @@ public interface BeanFactory {
 	 */
 	// 对FactoryBean的转义定义，提供获取FactoryBean的方式，
 	// 如果使用bean的名字检索FactoryBean得到的对象是工厂生成的对象
-	// 如果需要得到工厂本身，需要转移
+	// 如果需要得到工厂本身，需要转义
 	String FACTORY_BEAN_PREFIX = "&";
 
 
