90a574ef63f7ccfc7a88d2204205d2f41d85116b
lxcecho <37874882+lxcecho@users.noreply.github.com>
2023-01-15 23:49:22 +0800

    annotation debug.

--- simple-spring/src/main/java/com/xc/joy/AnnotationTest.java
+++ simple-spring/src/main/java/com/xc/joy/AnnotationTest.java
@@ -1,38 +1,40 @@
 package com.xc.joy;
 
 import com.xc.joy.aop.HelloService;
-import com.xc.joy.beans.Cat;
-import com.xc.joy.beans.Hello;
-import com.xc.joy.beans.Person;
-import com.xc.joy.beans.Student;
 import com.xc.joy.conf.AppConfig;
-import org.springframework.context.ApplicationContext;
+import com.xc.joy.model.Eman;
+import com.xc.joy.model.EmanConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
+ * 基于注解和基于 XML 配置的 Spring Bean 在创建时机上存在唯一的不同之处：
+ * 1. 基于 XML 配置的方式，Bean 对象的创建是在程序首次从工厂中获取该对象时才创建的；
+ * 2. 基于注解配置的方式，Bean 对象的创建是在注解处理器解析相应的 @Bean 注解时调用了该注解所修饰的方法，当该方法执行后，相应的对象自然就已经被创建出来了，
+ * 这时，Spring 就会将该对象纳入到工厂的管理范围之内，当我们首次尝试从工厂中获取到该 Bean 对象时，这时，该 Bean 对象实际上已经完成了创建并已被纳入到了
+ * 工厂的管理范围之内。
+ *
  * @author lxcecho 909231497@qq.com
  * @since 28.08.2021
  */
 public class AnnotationTest {
 
-	public static void main(String[] args) {
-		// 注解版 Spring 的用法，XXAware 调试
-		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main0(String[] args) {
+        // 注解版 Spring 的用法，XXAware 调试
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-		// factoryBean 获取
-		/*Hello bean = applicationContext.getBean(Hello.class);*/
+        // factoryBean 获取
+        /*Hello bean = applicationContext.getBean(Hello.class);*/
 
 		/*Person person = applicationContext.getBean(Person.class);
 		System.out.println(person);*/
 
-		// 打印所有注册到容器的 bean 实例
+        // 打印所有注册到容器的 bean 实例
 		/*String[] names = applicationContext.getBeanDefinitionNames();
 		for (String name : names) {
 			System.out.println(name);
 		}*/
 
-		// 测试 单实例引用非单实例，即 @Lookup
+        // 测试 单实例引用非单实例，即 @Lookup
 		/*Cat bean = applicationContext.getBean(Cat.class);
 		Cat bean1 = applicationContext.getBean(Cat.class);
 		System.out.println(bean == bean1);// false*/
@@ -45,19 +47,30 @@ public class AnnotationTest {
 		System.out.println(cat + "," + cat1);
 		System.out.println(stu1 + "," + stu2);*/
 
-		// 循环引用，原理测试
-		// AOP，原理测试
-		HelloService helloService = applicationContext.getBean(HelloService.class);
-		helloService.sayHello("lxcecho");
+        // 循环引用，原理测试
+        // AOP，原理测试
+        HelloService helloService = applicationContext.getBean(HelloService.class);
+        helloService.sayHello("lxcecho");
 
-	}
+    }
 
-	public static void main1(String[] args) {
-		// debug 实现 xxxAware 接口的赋值过程
-		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) {
+        // debug 实现 xxxAware 接口的赋值过程
+		/*AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 		Person person = applicationContext.getBean(Person.class);
 		ApplicationContext context = person.getContext();
-		System.out.println(context == applicationContext);
-	}
+		System.out.println(context == applicationContext);*/
+
+        // 相当于xml方式的工厂，内置了reader和scanner
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+        // 相当于xml的读取器要读取的位置
+        annotationConfigApplicationContext.register(EmanConfig.class);
+        // 刷新工厂
+        annotationConfigApplicationContext.refresh();
+        Eman eman = (Eman) annotationConfigApplicationContext.getBean("eman");
+
+        System.out.println(eman.getName() + "'s age is: " + eman.getAge());
+
+    }
 
 }
---
+++ simple-spring/src/main/java/com/xc/joy/model/Eman.java
@@ -0,0 +1,30 @@
+package com.xc.joy.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lxcecho 909231497@qq.com
+ * @since 21:54 15-01-2023
+ */
+public class Eman {
+
+    private int age;
+
+    private String name;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
